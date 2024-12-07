619b065c23933d61877cabdab06fe5378d8fdd48
juiceru228 <125103112+juiceru228@users.noreply.github.com>
2024-12-08 01:57:08 +1000

    Update ChinaPage.vue

--- client/src/components/ChinaPage.vue
+++ client/src/components/ChinaPage.vue
@@ -1,15 +1,6 @@
 <template>
 	<h1>Список тестовый</h1>
-	<div>
-  <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-    <b-dropdown-item>Первое действие</b-dropdown-item>
-    <b-dropdown-item>Второе действие</b-dropdown-item>
-    <b-dropdown-item>Третье действие</b-dropdown-item>
-    <b-dropdown-divider></b-dropdown-divider>
-    <b-dropdown-item active>Активное действие</b-dropdown-item>
-    <b-dropdown-item disabled>Отключенное действие</b-dropdown-item>
-  </b-dropdown>
-	</div>
+
 	<div>
 			{{ items }}
 	</div>
